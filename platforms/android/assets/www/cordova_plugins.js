862e4484e18661fad29d616a364df0ae82dab012
fsibapt <michele.reho@gmail.com>
2015-12-17 00:11:32 +0100

    Initial Build

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-extension": "1.2.3",
+    "cordova-plugin-admobpro": "2.10.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/guns.js
@@ -0,0 +1,362 @@
+guns =
+{
+    "submachineguns": {
+        "name": "Submachine Guns",
+        "guns": {
+            "kuda": {
+                "name": "Kuda",
+                "stats": {
+                    "damage": 6,
+                    "range": 11,
+                    "firerate": 10,
+                    "accuracy": 7,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 720
+            },
+            "vmp": {
+                "name": "VMP",
+                "stats": {
+                    "damage": 8,
+                    "range": 8,
+                    "firerate": 15,
+                    "accuracy": 3,
+                    "ammo": 40
+                },
+                "type": "auto",
+                "rpm": 900
+            },
+            "weevil": {
+                "name": "Weevil",
+                "stats": {
+                    "damage": 3,
+                    "range": 7,
+                    "firerate": 16,
+                    "accuracy": 8,
+                    "ammo": 50
+                },
+                "type": "auto",
+                "rpm": 720
+            },
+            "vesper": {
+                "name": "Vesper",
+                "stats": {
+                    "damage": 7,
+                    "range": 4,
+                    "firerate": 18,
+                    "accuracy": 5,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 1200
+            },
+            "pharo": {
+                "name": "Pharo",
+                "stats": {
+                    "damage": 8,
+                    "range": 8,
+                    "firerate": 11,
+                    "accuracy": 7,
+                    "ammo": 40
+                },
+                "type": "auto",
+                "rpm": 909
+            },
+            "razorback": {
+                "name": "Razorback",
+                "stats": {
+                    "damage": 4,
+                    "range": 13,
+                    "firerate": 6,
+                    "accuracy": 11,
+                    "ammo": 43
+                },
+                "type": "auto",
+                "rpm": 625
+            }
+        }
+    },
+    "assaultrifles": {
+        "name": "Assault Rifles",
+        "guns": {
+            "kn44": {
+                "name": "KN-44",
+                "stats": {
+                    "damage": 9,
+                    "range": 8,
+                    "firerate": 15,
+                    "accuracy": 5,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 650
+            },
+            "xr2": {
+                "name": "XR-2",
+                "stats": {
+                    "damage": 11,
+                    "range": 7,
+                    "firerate": 12,
+                    "accuracy": 7,
+                    "ammo": 30
+                },
+                "type": "burst",
+                "amount": 3
+            },
+            "hvk30": {
+                "name": "HVK-30",
+                "stats": {
+                    "damage": 6,
+                    "range": 5,
+                    "firerate": 17,
+                    "accuracy": 9,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 700
+            },
+            "icr1": {
+                "name": "ICR-1",
+                "stats": {
+                    "damage": 4,
+                    "range": 9,
+                    "firerate": 11,
+                    "accuracy": 13,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 600
+            },
+            "manowar": {
+                "name": "Man-O-War",
+                "stats": {
+                    "damage": 11,
+                    "range": 9,
+                    "firerate": 8,
+                    "accuracy": 9,
+                    "ammo": 30
+                },
+                "type": "auto",
+                "rpm": 517
+            },
+            "sheiva": {
+                "name": "Sheiva",
+                "stats": {
+                    "damage": 15,
+                    "range": 10,
+                    "firerate": 2,
+                    "accuracy": 10,
+                    "ammo": 20
+                },
+                "type": "semi"
+            },
+            "m8a7": {
+                "name": "M8A7",
+                "stats": {
+                    "damage": 5,
+                    "range": 11,
+                    "firerate": 7,
+                    "accuracy": 14,
+                    "ammo": 32
+                },
+                "type": "burst",
+                "amount": 4
+            }
+        }
+    },
+    "shotguns": {
+        "name": "Shotguns",
+        "guns": {
+            "krm262": {
+                "name": "KRM-262",
+                "stats": {
+                    "damage": 18,
+                    "range": 3,
+                    "firerate": 6,
+                    "accuracy": 3,
+                    "ammo": 8
+                },
+                "type": "semi"
+            },
+            "205brecci": {
+                "name": "205 Brecci",
+                "stats": {
+                    "damage": 10,
+                    "range": 5,
+                    "firerate": 8,
+                    "accuracy": 7,
+                    "ammo": 12
+                },
+                "type": "semi"
+            },
+            "haymaker12": {
+                "name": "Haymaker 12",
+                "stats": {
+                    "damage": 7,
+                    "range": 5,
+                    "firerate": 13,
+                    "accuracy": 5,
+                    "ammo": 16
+                },
+                "type": "auto",
+                "rpm": 350
+            },
+            "argus": {
+                "name": "Argus",
+                "stats": {
+                    "damage": 13,
+                    "range": 8,
+                    "firerate": 4,
+                    "accuracy": 5,
+                    "ammo": 10
+                },
+                "type": "semi"
+            }
+        }
+    },
+    "lightmachineguns": {
+        "name": "Light Machine Guns",
+        "guns": {
+            "brm": {
+                "name": "BRM",
+                "stats": {
+                    "damage": 9,
+                    "range": 11,
+                    "firerate": 7,
+                    "accuracy": 11,
+                    "ammo": 75
+                },
+                "type": "auto",
+                "rpm": 500
+            },
+            "dingo": {
+                "name": "Dingo",
+                "stats": {
+                    "damage": 7,
+                    "range": 8,
+                    "firerate": 14,
+                    "accuracy": 9,
+                    "ammo": 60
+                },
+                "type": "auto",
+                "rpm": 640
+            },
+            "gorgon": {
+                "name": "Gorgon",
+                "stats": {
+                    "damage": 12,
+                    "range": 9,
+                    "firerate": 6,
+                    "accuracy": 11,
+                    "ammo": 40
+                },
+                "type": "auto",
+                "rpm": 470
+            },
+            "48dredge": {
+                "name": "48 Dredge",
+                "stats": {
+                    "damage": 8,
+                    "range": 10,
+                    "firerate": 10,
+                    "accuracy": 10,
+                    "ammo": 60
+                },
+                "type": "burst",
+                "amount": 6
+            }
+        }
+    },
+    "sniperrifles": {
+        "name": "Sniper Rifles",
+        "guns": {
+            "drakon": {
+                "name": "Drakon",
+                "stats": {
+                    "damage": 8,
+                    "range": 14,
+                    "firerate": 8,
+                    "accuracy": 15,
+                    "ammo": 8
+                },
+                "type": "semi"
+            },
+            "locus": {
+                "name": "Locus",
+                "stats": {
+                    "damage": 14,
+                    "range": 14,
+                    "firerate": 3,
+                    "accuracy": 14,
+                    "ammo": 20
+                },
+                "type": "semi"
+            },
+            "p06": {
+                "name": "P-06",
+                "stats": {
+                    "damage": 18,
+                    "range": 14,
+                    "firerate": 1,
+                    "accuracy": 12,
+                    "ammo": 15
+                },
+                "type": "burst",
+                "amount": 3
+            },
+            "svg100": {
+                "name": "SVG-100",
+                "stats": {
+                    "damage": 16,
+                    "range": 14,
+                    "firerate": 2,
+                    "accuracy": 13,
+                    "ammo": 6
+                },
+                "type": "semi"
+            }
+        }
+    },
+    "pistols": {
+        "name": "Pistols",
+        "guns": {
+            "mr6": {
+                "name": "MR6",
+                "stats": {
+                    "damage": 12,
+                    "range": 9,
+                    "firerate": 5,
+                    "accuracy": 11,
+                    "ammo": 20
+                },
+                "type": "semi"
+            },
+            "rk5": {
+                "name": "RK5",
+                "stats": {
+                    "damage": 9,
+                    "range": 14,
+                    "firerate": 13,
+                    "accuracy": 11,
+                    "ammo": 15
+                },
+                "type": "burst",
+                "amount": 3
+            },
+            "lcar9": {
+                "name": "L-CAR 9",
+                "stats": {
+                    "damage": 7,
+                    "range": 6,
+                    "firerate": 16,
+                    "accuracy": 8,
+                    "ammo": 20
+                },
+                "type": "auto",
+                "rpm": 800
+            }
+        }
+    }
+}
+;
